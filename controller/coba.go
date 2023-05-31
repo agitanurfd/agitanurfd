7d53fd2e13553c9cfe0d32bad607052ae64c0f7f
Agitanurfd <agitanurfadillah45@gmail.com>
2023-05-31 15:09:32 +0700

    latihan11

--- controller/coba.go
+++ controller/coba.go
@@ -169,6 +169,17 @@ func GetAllUniversitas(c *fiber.Ctx) error {
 	return c.JSON(ps)
 }
 
+// InsertData godoc
+// @Summary Insert data presensi.
+// @Description Input data presensi.
+// @Tags Presensi
+// @Accept json
+// @Produce json
+// @Param request body Presensi true "Payload Body [RAW]"
+// @Success 200 {object} Presensi
+// @Failure 400
+// @Failure 500
+// @Router /ins [post]
 func InsertData(c *fiber.Ctx) error {
 	db := config.Ulbimongoconn
 	var presensi inimodul.Presensi
